92a5499bc0c6abf47d037bf00f78b9778b3d6177
DAcosta3103 <117118915+DAcosta3103@users.noreply.github.com>
2023-12-02 23:26:48 -0600

    Update datamanager.py

--- datamanager.py
+++ datamanager.py
@@ -1,20 +1,20 @@
 import time
 import random
-from eventos import EventManager  # Importa la clase EventManager de eventos.py
+from eventos import EventManager  # Acá se importa la clase EventManager del archivo brindado eventos.py
 
 class RealTimeDataManager:
     def __init__(self, event_manager):
-        self.data = {"temperatura": 25.0, "humedad": 60.0}  # Inicializa los datos.
-        self.event_manager = event_manager  # Almacena la instancia de EventManager.
+        self.data = {"temperatura": 25.0, "humedad": 60.0}  # Se inicializan los datos
+        self.event_manager = event_manager  # Se almacena la instancia de EventManager en event_manager
 
     def start_real_time_updates(self):
-        # Comienza a generar actualizaciones de datos en tiempo real.
+        # En este ciclo se generan actualizaciones de los datos, esto en tiempo real
         while True:
-            time.sleep(3)  # Espera 3 segundos entre actualizaciones.
-            self.generate_real_time_data()  # Genera y notifica los datos actualizados.
-
+            time.sleep(3)  # Se settea el tiempo de actualizaciones a 3 segundos
+            self.generate_real_time_data()  # Como el comando lo dice, se utiliza para generar  y notificar los datos actualizados
+    
     def generate_real_time_data(self):
-        # Actualiza los valores de temperatura y humedad de manera aleatoria.
+        # Los siguientes dos comandos se encargan de actualizar los valores de temperatura y humedad de manera aleatoria
         self.data["temperatura"] += random.uniform(-1.0, 1.0)
         self.data["humedad"] += random.uniform(-2.0, 2.0)
-        self.event_manager.notify("data_updated", self.data)  # Notifica a los suscriptores del cambio.
+        self.event_manager.notify("data_updated", self.data)  # Se notifican los cambios a los suscriptores
